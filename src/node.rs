0376b8215ed9befe01504c51189fe233f4d8f058
jovan <jdmitrovic@gmail.com>
2021-11-23 15:22:35 +0100

    Changed internal structure of Inner nodes

--- src/node.rs
+++ src/node.rs
@@ -1,22 +1,26 @@
 use crate::ARTKey;
 
-pub type ARTLink<K, V> = Option<Box<ARTNode<K, V>>>;
-
 pub enum ARTNode<K: ARTKey, V> {
-    Inner(ARTInnerNode<K, V>, Vec<u8>),
-    Leaf(ARTLeaf<K, V>),
+    Inner(ARTInnerNode<K, V>),
+    Leaf(Box<ARTLeaf<K, V>>),
 }
 
+pub type ARTLink<K, V> = Option<ARTNode<K, V>>;
+
 pub enum ARTInnerNode<K: ARTKey, V> {
-    Inner4 {
-        keys: [Option<u8>; 4],
-        children: [ARTLink<K, V>; 4],
-    },
-    // Inner16(),
-    // Inner48(),
-    Inner256 {
-        children: [ARTLink<K, V>; 256],
-    },
+    Inner4(ARTInner4),
+    Inner256(ARTInner256),
+}
+
+pub struct ARTInner4<K, V> {
+    partial_key: Vec<u8>,
+    keys: [Option<u8>; 4],
+    children: [ARTLink<K, V>; 4],
+}
+
+pub struct ARTInner256<K, V> {
+    partial_key: Vec<u8>,
+    children: [ARTLink<K, V>; 256],
 }
 
 pub struct ARTLeaf<K: ARTKey, V>{
@@ -35,37 +39,106 @@ impl<K: ARTKey, V> ARTLeaf<K, V> {
 
 impl<K: ARTKey, V> ARTInnerNode<K, V> {
     fn new_inner_4() -> ARTInnerNode<K, V> {
-        ARTInnerNode::Inner4 {
+        ARTInnerNode::Inner4(ARTInner4 {
             keys: [None; 4],
             children: [None; 4],
-        }
+            partial_key: vec![],
+        })
     }
 
     fn new_inner_256() -> ARTInnerNode<K, V> {
-        ARTInnerNode::Inner256 {
+        ARTInnerNode::Inner256(ARTInner256 {
             children: [None; 256],
-        }
+            partial_key: vec![],
+        })
     }
 
-    fn add_child(&mut self, key: K, value: V, key_byte: u8) -> &ARTLeaf<K, V> {
-        self.add_node(Box::new(ARTNode::Leaf(ARTLeaf::new(key, value))), key_byte)
+    pub fn add_child(&mut self, key: K, value: V, key_byte: u8) -> &ARTLeaf<K, V> {
+        self.add_node(ARTNode::Leaf(Box::new(ARTLeaf::new(key, value))), key_byte)
     }
 
-    fn add_node(&mut self, new_node: ARTNode<K, V>, key_byte: u8) -> &ARTNode<K, V> {
+    pub fn add_node(&mut self, new_node: &ARTNode<K, V>, key_byte: u8) -> &ARTNode<K, V> {
         match self {
-            ARTInnerNode::Inner4 { keys, children } => {
-                for (key, child) in self.keys.zip(self.children) {
+            ARTInnerNode::Inner4(node) => {
+                for (i, key) in node.keys.iter().enumerate() {
                     if key != None { continue; }
 
                     key.insert(key_byte);
-                    child.insert(new_node)
+                    node.children[i].insert(new_node);
+                    &node.children[i]
                 }
 
                // node.grow()
             }
-            ARTInnerNode::Inner256 { children } => {
-                children[key_byte].insert(new_node)
+            ARTInnerNode::Inner256(node) => {
+                node.children[key_byte].insert(new_node);
+                &node.children[key_byte]
             },
         }
+    }
+
+    pub fn iter(&self, key: K) -> Iter<K, V> {
+        Iter {
+            next: Some(self),
+            key_bytes: key.convert_to_bytes(),
+            depth: 0,
+        }
+    }
+}
+
+pub struct Iter<'a, K, V> {
+    next: Option<&'a ARTInnerNode<K, V>>,
+    key_bytes: Vec<u8>,
+    depth: u32,
+}
+
+impl<'a, K, V> Iterator for Iter<'a, K, V> {
+    type Item = &'a ARTInnerNode<K, V>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|inner_node| {
+            match inner_node {
+                ARTInnerNode::Inner4(node) => {
+                    let pk = &self.key_bytes[self.depth..node.partial_key.len()];
+                    if node.partial_key.iter().zip(pk).all(|a, b| a == b) == false {
+                        self.next = None;
+                        None
+                    }
+
+                    self.depth += node.partial_key.len();
+                    let key_byte = self.key_bytes[self.depth];
+                    let i = node.keys.iter().position(|k| k == key_byte);
+
+                    if let Some(pos) = i {
+                        self.depth += 1;
+
+                    } else {
+                        self.next = None;
+                    }
+
+                    &node
+                }
+                ARTInnerNode::Inner256(node) => {
+                    let pk = &self.key_bytes[self.depth..node.partial_key.len()];
+                    if node.partial_key.iter().zip(pk).all(|a, b| a == b) == false {
+                        self.next = None;
+                        None
+                    }
+
+                    let key_byte: u8 = self.key_bytes[self.depth];
+
+                    self.depth += node.partial_key.len() + 1;
+                    match node.children[key_byte] {
+                        None => self.next = None,
+                        Some(node) => {
+                            match node {
+                                ARTNode::Leaf(_) => None,
+                                ARTNode::Inner(inner_node) => self.next = inner_node,
+                            }
+                        }
+                    }
+                }
+            }
+        })
     }
 }
--- src/tree.rs
+++ src/tree.rs
@@ -1,23 +1,29 @@
 use crate::{ ARTree, ARTKey };
-use crate::node::{ ARTLink, ARTNode, ARTLeaf, ARTInnerNode};
+use crate::node::{ ARTNode, ARTLeaf, ARTInnerNode };
 
-impl ARTree {
+impl<K, V> ARTree<K, V> {
     pub fn new() -> Self {
         ARTree {
             root: None,
         }
     }
 
-    pub fn insert<K: ARTKey, V>(&mut self, key: K, value: V) {
+    pub fn insert(&mut self, key: K, value: V) {
         match self.root {
             None => self.root.insert(Box::new(ARTNode::Leaf(ARTLeaf::new(key, value)))),
-            Some(node) => {
+            Some(mut node) => {
+                let key_bytes = key.convert_to_bytes();
+
                 match node {
                     ARTNode::Leaf(_) => {
-                        let old_root = self.root.replace(ARTInnerNode::<K, V>::new_inner_4());
-                        return
+                        let new_root = ARTInnerNode::<K, V>::new_inner_4();
+                        let old_root = self.root.take();
+                        new_root.add_node(old_root, old_root.byte_key);
+                        new_root.add_child(key, value, key_bytes[0]);
+                        self.root.insert(Box::new(ARTNode::Inner(new_root)));
                     }
-                    ARTNode::Inner(node) => {}
+                    ARTNode::Inner(node) => {
+                    }
                 }
             }
         }
